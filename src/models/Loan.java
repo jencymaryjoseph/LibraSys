e79d7e61b562f17100961a2f3276f344df9b748c
jencymaryjoseph <35571282+jencymaryjoseph@users.noreply.github.com>
2024-07-01 19:24:43 +0530

    models

--- src/models/Loan.java
+++ src/models/Loan.java
@@ -1,5 +1,51 @@
 package models;
 
+import java.util.Date;
+
 public class Loan {
-    
+    private int userId;
+    private int bookId;
+    private Date loanDate;
+    private Date returnDate;
+
+    // Constructor
+    public Loan(int userId, int bookId, Date loanDate, Date returnDate) {
+        this.userId = userId;
+        this.bookId = bookId;
+        this.loanDate = loanDate;
+        this.returnDate = returnDate;
+    }
+
+    // Getters and Setters
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(Date loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
 }
--- src/models/User.java
+++ src/models/User.java
@@ -1,5 +1,61 @@
 package models;
 
+import java.util.LinkedList;
+
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    private LinkedList<Book> borrowedBooks;
+
+    // Constructor
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.borrowedBooks = new LinkedList<>();
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LinkedList<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(LinkedList<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
     
+    // Add book to borrowed list
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    // Remove book from borrowed list
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
 }
